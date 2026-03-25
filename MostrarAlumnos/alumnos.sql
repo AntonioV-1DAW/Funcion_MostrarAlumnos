469d84d3094a96f8b4c9ba911253dd558e76f9af
AntonioV-1DAW <antoniovillarealmoreno.guadalupe@alumnado.fundacionloyola.net>
2026-03-25 13:08:19 +0100

    Delete MostrarAlumnos directory

--- MostrarAlumnos/alumnos.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO alumnos (nombre) VALUES ('Juan Alberto Sanchez');
-INSERT INTO alumnos (nombre) VALUES ('María del Carmen Perez');
-INSERT INTO alumnos (nombre) VALUES ('Jesús Calleja');
-INSERT INTO alumnos (nombre) VALUES ('Laura Pombo');
-INSERT INTO alumnos (nombre) VALUES ('Eduardo Ginés');alumnos
