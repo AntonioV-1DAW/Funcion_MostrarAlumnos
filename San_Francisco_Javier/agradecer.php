5dc11ab502349e47a17b0dbffd6eeecec23c9bd7
AntonioV-1DAW <antoniovillarealmoreno.guadalupe@alumnado.fundacionloyola.net>
2026-03-25 21:56:45 +0100

    Add files via upload

---
+++ San_Francisco_Javier/agradecer.php
@@ -0,0 +1,55 @@
+<?php
+    include'configdb.php';
+    
+    function conectar(){
+        $conexion=new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+        return $conexion;
+    }
+
+    function mostrarAlumnos($conexion){
+        $conexion=conectar();
+        $sql="SELECT idAlumno, nombre FROM alumnos";
+        $resultado=$conexion->query($sql);
+
+        while($fila=$resultado->fetch_array()){
+            echo '<option  value="'.$fila['idAlumno'].'">';
+            echo $fila['idAlumno'].'-'.$fila['nombre'];
+            echo '</option>';
+        }
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="author" content="San Francisco Javier">
+        <link rel="StyleSheet" href="./Estilos.css" type="text/css"/>
+    </head>
+    <body>
+        <header id="Agradecer">
+            <img src="./imagenes/titulo.png"/>
+        </header>
+        <nav>
+			<a class="botones" href="./agradecer.php">Agradecer</a>
+			<a class="botones" href="./MisAgradecimientos.html">Mis Agradecimientos</a>
+            <a class="botones" href="./cerrar_sesion.php">Cerrar sesión</a>
+        </nav>
+        <section class="principal">
+            <label for="alumnos" placeholder="Selecciona un alumno">Para: </label>
+            <select name="alumnos" id="alumnos">
+                <?php
+                    mostrarAlumnos($conexion);
+                ?>
+            </select>
+            <label for="informacion">Agradecimiento:</label><br>
+            <input type="text" id="informacion" name="informacion" placeholder="Introduce tu mensaje" class="mensaje" required><br><br>
+        </section>
+        <aside>
+            <img src="./imagenes/jesuita.png" class="jesuita"/>
+            <p>
+                <h4>San Francisco Javier:</h4>
+                <div>¿De qué le sirve al hombre ganar el mundo entero, si pierde su alma?</div>
+            </p>
+        </aside>
+    </body>
+</html>
---
+++ San_Francisco_Javier/configdb.php
@@ -0,0 +1,6 @@
+<?php
+    define("SERVIDOR",'esvirgua.com');
+    define("USUARIO",'user1daw_25');
+    define("PASSWORD",'f@E3IygnC3RWN[]1');
+    define("BBDD",'user1daw_DB1-25');
+?>
